c4a5f0901b25b04ee005803d961b8393730d6626
eldorahmed <eldorbek970304@gmail.com>
2024-05-17 21:37:04 +0500

    first modal ready

---
+++ app.js
@@ -0,0 +1,44 @@
+const showBtn = document.querySelector("#show-btn");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector("#hide-btn");
+const overlay = document.getElementById("overlay");
+const cinfirmBtn = document.querySelector(".confirm-btn");
+const removeHidden = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const addHidden = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+showBtn.addEventListener("click", () => {
+  removeHidden();
+});
+closeBtn.addEventListener("click", () => {
+  addHidden();
+});
+overlay.addEventListener("click", () => {
+  addHidden();
+});
+cinfirmBtn.addEventListener("click", () => {
+  addHidden();
+  alert("Your order has been recieved!");
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    addHidden();
+  }
+});
+
+const imgCup = document.querySelector(".coffee-imgc");
+const imgAme = document.querySelector(".coffee-imga");
+const numc = document.querySelector(".numc");
+const numa = document.querySelector(".numa");
+
+imgCup.addEventListener("click", () => {
+  +numc.textContent++;
+});
+imgAme.addEventListener("click", () => {
+  +numa.textContent++;
+});
